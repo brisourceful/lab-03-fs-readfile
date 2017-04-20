d4b45723b716977c16e904cb7dd3c432dd11334c
Brianna <briannaburrows@gmail.com>
2017-04-20 12:35:27 -0700

    created array to push data

--- lab-brianna/fs-module.js
+++ lab-brianna/fs-module.js
@@ -2,17 +2,22 @@
 
 const fs = require('fs');
 
-fs.readFile(`${__dirname}/data/one.txt`, function(err, data){
-  if(err) throw
-  console.error();
-})
+module.exports = function () {
+  let arrg =[];
+  fs.readFile(`${__dirname}/data/one.txt`, function(err, data){
+    if(err) throw err;
+    arrg.push(data.toString('hex', 0, 8));
 
-fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
-  if(err) throw err
-  console.log();
-})
+    fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
+      if(err) throw err;
+      arrg.push(data.toString('hex', 0, 8));
 
-fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
-  if(err) throw err
-  console.log();
-});
+      fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
+        if(err) throw err;
+        arrg.push(data.toString('hex', 0, 8));
+      });
+    });
+  });
+};
+
+module.exports();
---
+++ lab-brianna/test/fs-module-test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const expect = require('chai').expect;
+const buffer = require('buffer').buffer;
+const fs = require('fs');
+
+describe('fs module', function() {
+  describe('#readFile',function() {
