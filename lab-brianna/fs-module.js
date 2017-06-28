f4071310c3732431fded1637bc477d0bc46ad4a8
Brianna <briannaburrows@gmail.com>
2017-06-28 15:36:42 -0700

    completed test

--- lab-brianna/fs-module.js
+++ lab-brianna/fs-module.js
@@ -3,21 +3,26 @@
 const fs = require('fs');
 
 module.exports = function () {
-  let arrg =[];
+  let arrgArray =[];
+
   fs.readFile(`${__dirname}/data/one.txt`, function(err, data){
     if(err) throw err;
-    arrg.push(data.toString('hex', 0, 8));
+    arrgArray.push(data.toString('hex', 0, 8));
+    console.log(arrgArray)
 
     fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
       if(err) throw err;
-      arrg.push(data.toString('hex', 0, 8));
+      arrgArray.push(data.toString('hex', 0, 8));
+      console.log(arrgArray)
 
       fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
         if(err) throw err;
-        arrg.push(data.toString('hex', 0, 8));
+        arrgArray.push(data.toString('hex', 0, 8));
+        console.log(arrgArray);
       });
     });
   });
+  return arrgArray;
 };
 
 module.exports();
--- lab-brianna/test/fs-module-test.js
+++ lab-brianna/test/fs-module-test.js
@@ -1,8 +1,38 @@
 'use strict';
 
-const expect = require('chai').expect;
-const buffer = require('buffer').buffer;
 const fs = require('fs');
+const expect = require('chai').expect;
 
-describe('fs module', function() {
-  describe('#readFile',function() {
+const textArray = ['./data/one.txt', './data/two.txt', './data/three.txt'];
+ const hexArray = [];
+ const testData = ['4120776f6e646572','5468652071756963','4661722066617220'];
+
+ describe('fs-module', function(){
+   describe('#readFile', function(){
+     it('should produce an array and equal values', done => {
+       fs.readFile(textArray[0], (err, data) => {
+         if(err) throw err;
+         hexArray.push(data.toString('hex', 0, 8));
+         expect(hexArray[0]).to.equal(testData[0]);
+         done();
+       });
+     });
+     it('should produce an array and equal values', done => {
+       fs.readFile(textArray[1], (err, data) => {
+         if(err) throw err;
+         hexArray.push(data.toString('hex', 0, 8));
+         expect(hexArray[1]).to.equal(testData[1]);
+         done();
+       });
+     });
+
+      it('should produce an array and equal values', done => {
+        fs.readFile(textArray[2], (err, data) => {
+             if(err) throw err;
+             hexArray.push(data.toString('hex', 0, 8));
+             expect(hexArray[2]).to.equal(testData[2]);
+             done();
+        });
+      });
+    });
+  });
